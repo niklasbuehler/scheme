66ad0274cab504202669963ecde42556f6cbb77e
niklasbuehler <me@niklasbuehler.com>
2020-09-05 14:55:23 +0200

    Readd weak typing

--- Eval.hs
+++ Eval.hs
@@ -120,10 +120,17 @@ numericBinop op params        = mapM unpackNum params >>= return . Number . fold
 
 unpackNum :: LispVal -> ThrowsError Integer
 unpackNum (Number n) = return n
+unpackNum (String n) = let parsed = reads n in
+                           if null parsed
+                             then throwError $ TypeMismatch "number" $ String n
+                             else return $ fst $ parsed !! 0
+unpackNum (List [n]) = unpackNum n
 unpackNum notNum     = throwError $ TypeMismatch "number" notNum
 
 unpackStr :: LispVal -> ThrowsError String
 unpackStr (String s) = return s
+unpackStr (Number s) = return $ show s
+unpackStr (Bool s)   = return $ show s
 unpackStr notString  = throwError $ TypeMismatch "string" notString
 
 unpackBool :: LispVal -> ThrowsError Bool
